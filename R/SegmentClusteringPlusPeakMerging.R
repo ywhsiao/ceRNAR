55df1d2f764b364d09ff2e8ff26b13cc9b73bd6f
YI-WEN Hsiao <y.w.hsiao9419@gmail.com>
2022-06-12 20:13:35 +0800

    minor

--- R/SegmentClusteringPlusPeakMerging.R
+++ R/SegmentClusteringPlusPeakMerging.R
@@ -54,7 +54,7 @@ SegmentClusteringPlusPeakMerging <- function(path_prefix = NULL,
   ## create a cluster
   message('\u2605 Number of computational cores: ',parallel::detectCores()-3,'/',parallel::detectCores(), '.')
   #doParallel::registerDoParallel(1)
-  doParallel::registerDoParallel(parallel::detectCores()-3)
+  #doParallel::registerDoParallel(parallel::detectCores()-3)
   sigCernaPeak <- function(index,d, cor_threshold_peak, window_size){
     w <- window_size
     mir = mirna_total[index]
@@ -241,8 +241,8 @@ SegmentClusteringPlusPeakMerging <- function(path_prefix = NULL,
     tmp
   }
   # seed=NULL
-  # future::plan("future::cluster", workers=parallel::detectCores()-3)
-  testfunction <- purrr::map(1:length(mirna_total), sigCernaPeak,readRDS(paste0(project_name,'-',disease_name,'/02_potentialPairs/',project_name,'-',disease_name,'_pairfiltering.rds')),0.85,105)
+  future::plan("future::cluster", workers=parallel::detectCores()-3)
+  testfunction <- furrr::future_map(1:length(mirna_total), sigCernaPeak,readRDS(paste0(project_name,'-',disease_name,'/02_potentialPairs/',project_name,'-',disease_name,'_pairfiltering.rds')),0.85,105)
 
 
   FinalResult <- purrr::compact(testfunction)
