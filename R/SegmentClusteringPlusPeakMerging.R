f8a749616ec1fb3d7fabbd8aa6d8dd17274d01d3
YI-WEN Hsiao <y.w.hsiao9419@gmail.com>
2022-06-12 12:16:33 +0800

    minor

--- R/SegmentClusteringPlusPeakMerging.R
+++ R/SegmentClusteringPlusPeakMerging.R
@@ -64,8 +64,8 @@ SegmentClusteringPlusPeakMerging <- function(path_prefix = NULL,
 
       gene_pair <- combn(gene,2)
       total_pairs <- choose(length(gene),2)
+      print(paste0('which miRNA: ',index, ';total pairs: ', p))
       tmp <- foreach(p=1:total_pairs, .combine = "rbind")  %dopar%  {
-        print(paste0('which miRNA: ',index, ';which pairs: ', p))
         cand.ceRNA=c()
         location=list()
         r=gene_pair[1,p]
@@ -210,20 +210,18 @@ SegmentClusteringPlusPeakMerging <- function(path_prefix = NULL,
             N2 <- result$output[min_seg,"num.mark"]
             Test <- as.numeric(2*pnorm(abs(z1-z2)/sqrt(1/(N1-3)+1/(N2-3)),lower.tail = FALSE))
             # generate final output
-            if(!is.na(Test[1])){
-              if(Test[1] < 0.05){
-                if(sum(cand.corr[peak.loc+1] > cor_threshold_peak) >0 && sum(cand.corr[peak.loc+1] > cor_threshold_peak) <=2){  ### para 0.5
-                  cand.ceRNA=paste(r,s)
+            if(Test[1] < 0.05){
+              if(sum(cand.corr[peak.loc+1] > cor_threshold_peak) >0 && sum(cand.corr[peak.loc+1] > cor_threshold_peak) <=2){  ### para 0.5
+                cand.ceRNA=paste(r,s)
 
-                  peak.loc=sort(c(peak.loc,no_merg_loc)) #put back the peak that can't be merged
+                peak.loc=sort(c(peak.loc,no_merg_loc)) #put back the peak that can't be merged
 
-                  True_peak <- peak.loc[cand.corr[peak.loc+1] > cor_threshold_peak]
-                  location=result$output[True_peak,c("loc.start","loc.end")]
+                True_peak <- peak.loc[cand.corr[peak.loc+1] > cor_threshold_peak]
+                location=result$output[True_peak,c("loc.start","loc.end")]
 
-                  if(!is.null(cand.ceRNA)){
-                    lst <- list(miRNA=mir,cand.ceRNA=cand.ceRNA,location=location,numOfseg=result$output$num.mark[True_peak])
-                    lst
-                  }
+                if(!is.null(cand.ceRNA)){
+                  lst <- list(miRNA=mir,cand.ceRNA=cand.ceRNA,location=location,numOfseg=result$output$num.mark[True_peak])
+                  lst
                 }
               }
             }
